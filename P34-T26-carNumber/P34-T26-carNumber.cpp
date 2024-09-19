28ccccea6a3428ba0d92b8eb2a783a9de61bbecf
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-19 16:55:53 +0300

    main

--- P34-T26-carNumber/P34-T26-carNumber.cpp
+++ P34-T26-carNumber/P34-T26-carNumber.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include "CarNumber.h"
+
+int CarNumber::counter = 9985;
+char CarNumber::firstLetter = 'A';
+char CarNumber::secondLetter = 'Z';
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    CarNumber arr[55];
+	for (int i = 0; i < 55; i++)
+	{
+		if (i % 5 == 0) cout << endl;
+		cout << arr[i].getNumber() << "\t";
+	}
+	cout << "\n__________________________\n";
+	cout << "counter: " << CarNumber::getCounter() << endl;
+	cout << "first: " << CarNumber::getFirstLetter() << endl;
+	cout << "second: " << CarNumber::getSecondLetter() << endl;
+
+
 }
 
 
