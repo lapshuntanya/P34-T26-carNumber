63e0c63d090e5a0a357223c42804450f37d05759
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-19 17:17:17 +0300

    main

--- P34-T26-carNumber/P34-T26-carNumber.cpp
+++ P34-T26-carNumber/P34-T26-carNumber.cpp
@@ -1,12 +1,12 @@
-#include "CarNumber.h"
+#include "dia.h"
 
-int CarNumber::counter = 9985;
+int CarNumber::counter = 9997;
 char CarNumber::firstLetter = 'A';
 char CarNumber::secondLetter = 'Z';
 
 int main()
 {
-    CarNumber arr[55];
+   /* CarNumber arr[55];
 	for (int i = 0; i < 55; i++)
 	{
 		if (i % 5 == 0) cout << endl;
@@ -15,9 +15,22 @@ int main()
 	cout << "\n__________________________\n";
 	cout << "counter: " << CarNumber::getCounter() << endl;
 	cout << "first: " << CarNumber::getFirstLetter() << endl;
-	cout << "second: " << CarNumber::getSecondLetter() << endl;
+	cout << "second: " << CarNumber::getSecondLetter() << endl;*/
 
 
+	DIA Kharkiv;
+	cout << "List: " << endl;
+	Kharkiv.showInfo();
+	cout << "-----------------------------------\n";
+
+	string number;
+	cout << "Input number: "; cin >> number;
+	Kharkiv.takeCar(number);
+
+	//Kharkiv.showInfo();
+	for (int i = 0; i < Kharkiv.getAmountofCars(); i++)	{
+		cout << Kharkiv[i].getNumber() << endl;
+	}
 }
 
 
